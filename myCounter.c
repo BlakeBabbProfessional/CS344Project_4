b075b0e6c9382b16824b113da69d7050514c046a
blakebabb <leopard50x1@gmail.com>
2022-11-18 16:33:58 -0800

    did it

--- myCounter.c
+++ myCounter.c
@@ -8,11 +8,12 @@
 #define MAX 10
 
 int myCount;
+int canPrint = 0;
 pthread_mutex_t myMutex;
-// Less than max
-pthread_cond_t myCond1;
-// Greater than 0
-pthread_cond_t myCond2;
+// Can increment myCount
+pthread_cond_t myCond1 = PTHREAD_COND_INITIALIZER;
+// Can print myCount
+pthread_cond_t myCond2 = PTHREAD_COND_INITIALIZER;
 
 int main() {
 	printf("PROGRAM START\n"); fflush(stdout);
@@ -21,20 +22,19 @@ int main() {
 
 	pthread_mutex_init(&myMutex, NULL);
 
-	pthread_t producer;
+	pthread_t producer, consumer;
 	pthread_create(&producer, NULL, produce, NULL);
-
-	pthread_t consumer;
 	pthread_create(&consumer, NULL, consume, NULL);
 
-	usleep(10000);
+	pthread_join(producer, NULL);
+	pthread_join(consumer, NULL);
 
 	printf("PROGRAM END\n"); fflush(stdout);
 	return 0;
 }
 
 /*
- * Start routine for a thread to increment count to MAX
+ * Start routine for a thread to increment myCount to MAX
  */
 void* produce(void* argument) {
 	printf("PRODUCER THREAD CREATED\n"); fflush(stdout);
@@ -43,23 +43,27 @@ void* produce(void* argument) {
 		pthread_mutex_lock(&myMutex);
 		printf("PRODUCER: myMutex locked\n"); fflush(stdout);
 
-		printf("PRODUCER: waiting on myCond1\n"); fflush(stdout);
-		pthread_cond_wait(&myCond1, &myMutex);
+		while (canPrint) {
+			printf("PRODUCER: waiting on myCond1\n"); fflush(stdout);
+			pthread_cond_wait(&myCond1, &myMutex);
+		}
 
 		myCount++;
-
-		pthread_mutex_unlock(&myMutex);
-		printf("PRODUCER: myMutex unlocked\n"); fflush(stdout);
+		canPrint = 1;
 
 		pthread_cond_signal(&myCond2);
 		printf("PRODUCER: signaled myCond2\n"); fflush(stdout);
+
+		pthread_mutex_unlock(&myMutex);
+		printf("PRODUCER: myMutex unlocked\n"); fflush(stdout);
 	}
 
+	printf("PRODUCER THREAD ENDED\n"); fflush(stdout);
 	return NULL;
 }
 
 /*
- * Start routine for a thread to print out count
+ * Start routine for a thread to print out myCount
  */
 void* consume(void* argument) {
 	printf("CONSUMER THREAD CREATED\n"); fflush(stdout);
@@ -68,17 +72,21 @@ void* consume(void* argument) {
 		pthread_mutex_lock(&myMutex);
 		printf("CONSUMER: myMutex locked\n"); fflush(stdout);
 
-		printf("CONSUMER: waiting on myCond2\n"); fflush(stdout);
-		pthread_cond_wait(&myCond2, &myMutex);
+		while (!canPrint) {
+			printf("CONSUMER: waiting on myCond2\n"); fflush(stdout);
+			pthread_cond_wait(&myCond2, &myMutex);
+		}
 
 		printf("CONSUMER: %d -> %d\n", myCount-1, myCount); fflush(stdout);
-
-		pthread_mutex_unlock(&myMutex);
-		printf("CONSUMER: myMutex unlocked\n"); fflush(stdout);
+		canPrint = 0;
 
 		pthread_cond_signal(&myCond1);
 		printf("CONSUMER: signaled myCond1\n"); fflush(stdout);
+
+		pthread_mutex_unlock(&myMutex);
+		printf("CONSUMER: myMutex unlocked\n"); fflush(stdout);
 	}
 
+	printf("CONSUMER THREAD ENDED\n"); fflush(stdout);
 	return NULL;
 }
